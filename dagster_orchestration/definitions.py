0acf1d164d29b291e411dbfa5b0eddacc3e24b52
toniovi <131332847+toniovi@users.noreply.github.com>
2024-03-22 10:54:34 +0000

    Ready to test new feature

--- dagster_orchestration/definitions.py
+++ dagster_orchestration/definitions.py
@@ -5,14 +5,14 @@ import os
 
 from dagster import Definitions
 
-from .assets.from_events_data.events import the_s3_files_as_dict_of_dataframes, get_new_s3_files, materialize_the_assets_in_dagster
+from .assets.from_events_data.events import the_s3_files_as_dict_of_dataframes, get_new_s3_files
 from .sensors.sensors import check_for_new_s3_files
 #from .schedules import launch_sensor_every_five_minutes
 from .resources.resources import MyAWSS3Resource
 
 
 defs = Definitions(
-    assets=[the_s3_files_as_dict_of_dataframes, get_new_s3_files, materialize_the_assets_in_dagster],
+    assets=[the_s3_files_as_dict_of_dataframes, get_new_s3_files],
     sensors=[check_for_new_s3_files],
     resources={
         "s3_with_bucket": MyAWSS3Resource(
--- dagster_orchestration/sensors/sensors.py
+++ dagster_orchestration/sensors/sensors.py
@@ -1,7 +1,8 @@
 #TB! load env ariables
 import our_utilities; our_utilities.load_env_with_substitutions()
 
-from dagster import sensor, RunRequest, SensorEvaluationContext, AssetKey
+from dagster import (sensor, SensorResult, SensorEvaluationContext,
+                    RunRequest, AssetKey)
 from dagster.core.instance import DagsterInstance
 
 import os
@@ -14,14 +15,21 @@ def check_for_new_s3_files(context: SensorEvaluationContext,
     s3_client = s3_with_bucket.create_s3_client()
     bucket = s3_with_bucket.bucket_name
 
-    dagster_instance = DagsterInstance.get()
+    new_files = []
 
     paginator = s3_client.get_paginator('list_objects')
+
     for result in paginator.paginate(Bucket=bucket):
         for file in result.get('Contents', []):
             file_name = file['Key']
-            if not dagster_instance.has_asset_key(AssetKey(file_name)):
-                yield RunRequest(run_key=None, run_config={})
+            if not context.instance.has_dynamic_partition(files_partitions_def.name, file_name):
+                new_files.append(file_name)
+
+    # Return DynamicPartitionsDefinition to generate new partitions for new files
+    return SensorResult(
+        run_requests=[RunRequest(partition_key=file_name) for file_name in new_files],
+        dynamic_partitions_requests=[files_partitions_def.build_add_request(new_files)],
+    )
 
 # In this code:
 
